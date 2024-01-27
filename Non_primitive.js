2b30c2a5c436e85a9dc955be3d4ce0570dca30a8
Vaishnavi Mishra <mishrariya.1508@gmail.com>
2024-01-27 13:18:03 +0530

    Non Primitive types

---
+++ Non_primitive.js
@@ -0,0 +1,14 @@
+const heros = ["Elle", "Nick", "Billy"];
+let myObj = {
+    name: "Vaish",
+    age: 22,
+    isApproved: true,
+}
+
+const my_function = function() {
+    console.log("Hello World!")
+}
+
+    console.log(typeof heros)
+    console.log(typeof myObj)
+    console.log(typeof my_function)
